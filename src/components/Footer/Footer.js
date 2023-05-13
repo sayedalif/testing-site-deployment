417830a401380bd5d0b55d6c79c28aee25ab1834
sayedalif <sayedalif18@gmail.com>
2023-05-14 00:40:26 +0600

    no pun intended

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className='text-center'>
-      <h1>copyright protected pun intended 😂</h1>
+      <h1>copyright protected no pun intended 😂</h1>
     </div>
   );
 };
